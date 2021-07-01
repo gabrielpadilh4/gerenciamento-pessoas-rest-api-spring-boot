8724d9f853a41dbacaf72d6b5282fc8f379fa874
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-07-01 19:33:04 -0300

    Initial Commit

---
+++ src/test/java/com/github/gabrielpadilh4/personapi/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabrielpadilh4.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
